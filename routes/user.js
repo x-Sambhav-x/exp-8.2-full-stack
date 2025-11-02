8903a2be8d7717ed565bb3757e0e7eaf0ea0c9f7
x-Sambhav-x <nikhrasambhav@gmail.com>
2025-11-02 23:15:10 +0530

    Delete routes directory

--- routes/user.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { verifyToken } from "../middleware/auth.js";
-const router = express.Router();
-
-router.get("/profile", verifyToken, (req, res) => {
-  res.json({ message: "Access Granted ✅", user: req.user });
-});
-
-export default router;
